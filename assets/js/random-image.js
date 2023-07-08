a1687a1b66fb7feac84dd65e558dbc615b9062b0
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-07-07 22:48:20 -0400

    Cropped pictures, made random image script better
    Cropped pictures so that they are all squares.
    
    Modified random image script. The image displayed is still random, however, if the aspect ratio is too small (i.e., width / height ratio gets smaller), the image is not displayed at all. I believe this is best for formatting purposes and leaves a clean look on mobile devices, while at the same time displaying pictures on more stationary devices like computers.

--- assets/js/random-image.js
+++ assets/js/random-image.js
@@ -1,20 +1,29 @@
-rand = Math.floor(Math.random() * 5) + 1;
+viewWidth = window.innerWidth;
+viewHeight = window.innerHeight;
 
-pictDir = "/assets/images/random-image/image" + rand + ".jpg";
-
-
-if (rand == 1) {
-    altText = "A headshot of me"
-} else if (rand == 2) {
-    altText = "Me standing next to a sign that says \"Freistaat Bayern\"(German for \"Free State of Bavaria\") on top of the Zugspitze"
-} else if (rand == 3) {
-    altText = "A picture of me after my high school graduation"
-} else if (rand == 4) {
-    altText = "Me standing outside of the Clarice Smith Performing Arts Center after my final trumpet jury"
+if (viewWidth / viewHeight < 1000 / 720) {
+    document.getElementById("img-parent").remove();
 } else {
-    altText = "Me standing with the Baltimore Orioles' mascot during a postseason game"
-}
+    console.log()
 
-ele = document.getElementById('random-image');
-ele.src = pictDir;
-ele.alt = altText;
+    rand = Math.floor(Math.random() * 5) + 1;
+
+    pictDir = "/assets/images/random-image/image" + rand + ".jpg";
+
+
+    if (rand == 1) {
+        altText = "A headshot of me"
+    } else if (rand == 2) {
+        altText = "Me standing next to a sign that says \"Freistaat Bayern\"(German for \"Free State of Bavaria\") on top of the Zugspitze"
+    } else if (rand == 3) {
+        altText = "A picture of me after my high school graduation"
+    } else if (rand == 4) {
+        altText = "Me standing outside of the Clarice Smith Performing Arts Center after my final trumpet jury"
+    } else {
+        altText = "Me standing with the Baltimore Orioles' mascot during a postseason game"
+    }
+
+    ele = document.getElementById('random-image');
+    ele.src = pictDir;
+    ele.alt = altText;
+}
