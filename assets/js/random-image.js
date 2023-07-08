0456805091cf94a70e65f606bee2206ab109eab3
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-07-07 22:21:48 -0400

    Fixed random image script
    Did not realize that GitHub Pages does not handle get requests. Hardcoded in some values to produce the intended effect. Values will need to be updated if images are updated.

--- assets/js/random-image.js
+++ assets/js/random-image.js
@@ -1,22 +1,20 @@
-$.getJSON('/assets/images/random-image', imgs => {
-    console.log(imgs);
+rand = Math.floor(Math.random() * 5) + 1;
 
-    rand = Math.floor(Math.random() * imgs.length);
-    pictDir = "/assets/images/random-image/" + imgs[rand];
+pictDir = "/assets/images/random-image/image" + rand + ".jpg";
 
-    $.getJSON('/assets/images/random-image-alt-text', txt => {
-        altFile = "/assets/images/random-image-alt-text/" + txt[rand];
 
-        $.get(altFile, altText => {
-            ele = document.getElementById('random-image');
-            ele.src = pictDir;
-            ele.alt = altText;
+if (rand == 1) {
+    altText = "A headshot of me"
+} else if (rand == 2) {
+    altText = "Me standing next to a sign that says \"Freistaat Bayern\"(German for \"Free State of Bavaria\") on top of the Zugspitze"
+} else if (rand == 3) {
+    altText = "A picture of me after my high school graduation"
+} else if (rand == 4) {
+    altText = "Me standing outside of the Clarice Smith Performing Arts Center after my final trumpet jury"
+} else {
+    altText = "Me standing with the Baltimore Orioles' mascot during a postseason game"
+}
 
-            console.log(ele.attributes.src);
-            console.log(ele.attributes.alt);
-
-        })
-
-    });
-
-});
+ele = document.getElementById('random-image');
+ele.src = pictDir;
+ele.alt = altText;
