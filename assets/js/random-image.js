bde37fef2fdb79de03b9252b939294a3b8569d85
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-07-08 11:24:10 -0400

    Fixed random image text, updated random image text
    One of them didn't have a space between an internal quotation mark and another character. The other two were updated to be more concise.

--- assets/js/random-image.js
+++ assets/js/random-image.js
@@ -10,13 +10,12 @@ if (viewWidth / viewHeight < 1000 / 720) {
 
     pictDir = "/assets/images/random-image/image" + rand + ".jpg";
 
-
     if (rand == 1) {
-        altText = "A headshot of me"
+        altText = "My professional headshot"
     } else if (rand == 2) {
-        altText = "Me standing next to a sign that says \"Freistaat Bayern\"(German for \"Free State of Bavaria\") on top of the Zugspitze"
+        altText = "Me standing next to a sign that says \"Freistaat Bayern\" (German for \"Free State of Bavaria\") on top of the Zugspitze"
     } else if (rand == 3) {
-        altText = "A picture of me after my high school graduation"
+        altText = "Me after my high school graduation"
     } else if (rand == 4) {
         altText = "Me standing outside of the Clarice Smith Performing Arts Center after my final trumpet jury"
     } else {
