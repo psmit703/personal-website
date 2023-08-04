01ec58940e46c4c41b0ed018a3cfaaa4d9f0aeda
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-08-04 15:35:50 -0400

    Adjusted navbar scroll feature to accommodate mouse movements
    navbar-scroll.js - the navbar now appears when the mouse is over the navbar element. It subsequently disappears when the mouse moves away from the element. However, with respect to mouse movement, the navbar only disappears if the new mouse location is visually lower than the previous mouse location. This allows for making the navbar appear via scrolling up, even when the mouse is not at the top, and keeping it visible while moving the mouse to it.

--- assets/js/navbar-scroll.js
+++ assets/js/navbar-scroll.js
@@ -1,18 +1,44 @@
 let navBar = document.getElementsByClassName("navbar")[0];
-
 let dropdown = document.getElementById("navbar-options");
 
 let prevScrollPos = window.scrollY;
+
+let prevMouseY = undefined;
+let mouseY = undefined;
+let mouseAtTop = false;
 
 window.onscroll = function () {
     let currentScrollPos = window.scrollY;
     if (prevScrollPos > currentScrollPos) {
         navBar.style.top = "0";
     } else {
-        if (dropdown.classList.contains("show") == false) {
+        if (dropdown.classList.contains("show") == false && mouseAtTop == false) {
             navBar.style.top = "-100px";
         }
     }
     prevScrollPos = currentScrollPos;
 }
 
+window.onmousemove = function (event) {
+    mouseY = event.clientY
+    if (prevMouseY == undefined) {
+        prevMouseY = mouseY;
+    }
+
+    if (mouseY <= 124) {
+        mouseAtTop = true;
+    } else {
+        mouseAtTop = false;
+    }
+
+    if (mouseAtTop || prevScrollPos == 0 || dropdown.classList.contains("show")
+        || dropdown.classList.contains("collapsing")) {
+        console.log(prevScrollPos, window.scrollY)
+        navBar.style.top = "0";
+    } else if (mouseY > prevMouseY) {
+        navBar.style.top = "-100px";
+        console.log("hidden")
+    }
+
+    prevMouseY = mouseY;
+}
