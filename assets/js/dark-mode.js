10d37586db9e36ebc7638b747bf51fa33343a60c
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-08-06 01:56:46 -0400

    Added dark mode features
    (Mostly) done for index.html - each page will need some custom stuff. Started adding cookie support for remembering dark mode preference.

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -5,6 +5,7 @@ darkModeOn = false;
 // just using true for testing purposes
 // will change condition when completed and/or as needed
 // light mode is default
+
 if (true) {
     toDarkMode();
     // change icon without animation
@@ -12,21 +13,71 @@ if (true) {
 }
 
 function toDarkMode() {
+    navbar = document.getElementById("navbar");
+    navbar.classList.remove("navbar-light");
+    navbar.classList.remove("bg-light");
+    navbar.classList.add("navbar-dark");
+    navbar.classList.add("bg-dark");
 
+    document.body.classList.add("bg-dark");
+    document.body.classList.add("text-light");
+
+    document.getElementById("footer").classList.remove("text-muted");
+    document.getElementById("footer").classList.add("text-light");
+
+    let langSVGs = document.getElementsByClassName("langSVG");
+    for (let i = 0; i < langSVGs.length; i++) {
+        langSVGs[i].height = "90";
+        langSVGs[i].width = "90";
+        langSVGs[i].style.objectFit = "contain";
+        langSVGs[i].style.backgroundColor = "#888888";
+        langSVGs[i].style.padding = "10px";
+        langSVGs[i].style.borderRadius = "10px";
+    }
+
+    let contactSVGs = document.getElementsByClassName("contactSVG");
+    for (let i = 0; i < contactSVGs.length; i++) {
+        contactSVGs[i].style.color = "rgb(248, 249, 250)";
+    }
 }
 
 function toLightMode() {
+    navbar = document.getElementById("navbar");
+    navbar.classList.remove("navbar-dark");
+    navbar.classList.remove("bg-dark");
+    navbar.classList.add("navbar-light");
+    navbar.classList.add("bg-light");
 
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("text-light");
+
+    document.getElementById("footer").classList.remove("text-light");
+    document.getElementById("footer").classList.add("text-muted");
+
+    let langSVGs = document.getElementsByClassName("langSVG");
+    for (let i = 0; i < langSVGs.length; i++) {
+        langSVGs[i].height = "70";
+        langSVGs[i].width = "70";
+        langSVGs[i].style.objectFit = "contain";
+        langSVGs[i].style.backgroundColor = "transparent";
+        langSVGs[i].style.padding = "0";
+        langSVGs[i].style.borderRadius = "0";
+    }
+
+    let contactSVGs = document.getElementsByClassName("contactSVG");
+    for (let i = 0; i < contactSVGs.length; i++) {
+        contactSVGs[i].style.color = "black";
+    }
 }
 
 darkModeBtn.addEventListener("click", function () {
     if (darkModeOn) {
         toLightMode();
-        console.log("light mode")
         darkModeOn = false;
+        document.cookie = "darkModeOn=false";
     } else {
         toDarkMode();
-        console.log("dark mode")
         darkModeOn = true;
+        document.cookie = "darkModeOn=true";
     }
 });
