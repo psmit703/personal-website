c5af4693c43c3c08485b8113fa933dd9879d3e64
Pete Smith <105141656+psmit703@users.noreply.github.com>
2023-08-07 08:20:22 -0400

    Minor bug fix and QoL feature
    QoL feature - made dark mode button appear on scroll up for all formats. Previously was just for mobile formats, but I think I like this better.
    
    Bug fix - scope issue I think, should be fixed

--- assets/js/navbar-scroll.js
+++ assets/js/navbar-scroll.js
@@ -4,7 +4,6 @@ let darkModeWrapper = document.getElementById("dark-mode-btn");
 let darkModeWrapperVisible = true;
 
 let viewportWidth = document.documentElement.clientWidth
-let navWidth = document.defaultView.getComputedStyle(navbar).width
 
 let prevScrollPos = window.scrollY;
 
@@ -24,10 +23,8 @@ window.onscroll = function () {
 
         if (prevScrollPos > currentScrollPos) {
             navbar.style.top = "0";
-            if (window.scrollY == 0 || navWidth < 768) {
-                darkModeWrapper.style.animation = "dark-mode-scroll-visible 0.3s ease-in-out forwards";
-                darkModeWrapperVisible = true;
-            }
+            darkModeWrapper.style.animation = "dark-mode-scroll-visible 0.3s ease-in-out forwards";
+            darkModeWrapperVisible = true;
         } else {
             if (dropdown.classList.contains("show") == false && mouseAtTop == false) {
                 navbar.style.top = "-100px";
@@ -46,6 +43,8 @@ window.onscroll = function () {
 
 window.onmousemove = function (event) {
     if (animationActive == false) {
+        let navWidth = document.defaultView.getComputedStyle(navbar).width
+
         navWidth = navWidth.substring(0, navWidth.length - 2)
         navWidth = parseInt(navWidth)
 
