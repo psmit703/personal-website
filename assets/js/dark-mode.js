cb75f56375364405cf618ae9214a8101206f6b0a
Pete Smith <105141656+psmit703@users.noreply.github.com>
2024-02-19 20:07:10 -0500

    stylization updates for index.html

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -90,12 +90,8 @@ function toDarkMode() {
 
     let langSVGs = document.getElementsByClassName("langSVG");
     for (let i = 0; i < langSVGs.length; i++) {
-        langSVGs[i].height = "90";
-        langSVGs[i].width = "90";
         langSVGs[i].style.objectFit = "contain";
         langSVGs[i].style.backgroundColor = "#888888";
-        langSVGs[i].style.padding = "10px";
-        langSVGs[i].style.borderRadius = "10px";
     }
 
     let contactSVGs = document.getElementsByClassName("contactSVG");
@@ -152,12 +148,8 @@ function toLightMode() {
 
     let langSVGs = document.getElementsByClassName("langSVG");
     for (let i = 0; i < langSVGs.length; i++) {
-        langSVGs[i].height = "70";
-        langSVGs[i].width = "70";
         langSVGs[i].style.objectFit = "contain";
         langSVGs[i].style.backgroundColor = "transparent";
-        langSVGs[i].style.padding = "0";
-        langSVGs[i].style.borderRadius = "0";
     }
 
     let contactSVGs = document.getElementsByClassName("contactSVG");
